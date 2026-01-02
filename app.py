db9b801445204e5cad7435f8e173c78e74eab329
srivarshans2006 <srivarshangs@gmail.com>
2026-01-02 13:59:03 +0530

    Chatbot created sucessfully

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask ,render_template,request,redirect,url_for,session
 import sqlite3
 import pandas as pd
 from werkzeug.security import generate_password_hash,check_password_hash
+import ollama
+from flask import jsonify
 
 app=Flask(__name__)
 app.secret_key="secret123"
@@ -88,6 +90,42 @@ def dashboard():
 
     return render_template("dashboard.html",category_sales=category_sales,monthly_sales=monthly_sales)
 
+@app.route("/chat")
+def chat_page():
+    return render_template("chatbot.html")
+
+
+@app.route("/chat-ai",methods=["POST"])
+
+def chat_ai():
+    user_question=request.json.get("question")
+
+    #Loading sales data
+    conn=sqlite3.connect("sales.db")
+    df=pd.read_sql_query("SELECT * FROM sales",conn)
+    conn.close()
+
+    if df.empty:
+        return jsonify({"answer":"No sales data available yet."})
+    
+    df["total"]=df["quantity"]*df["price"]
+
+    category_sales=df.groupby("category")["total"].sum().to_dict()
+    monthly_sales=df.groupby(df["date"].str[:7])["total"].sum().to_dict()
+
+    data_summary=f""" Category wise sales : {category_sales} 
+     Monthly sales : {monthly_sales} """
+    
+    prompt=f""" You are a sales analyst chatbot.
+
+    Data:{data_summary}
+    Question:{user_question}
+
+    Answer clearly in simple words. """
+
+    response=ollama.chat(model="llama3",messages=[{"role":"user","content":prompt}])
+
+    return jsonify({"answer":response["message"]["content"]})
 
 
 
